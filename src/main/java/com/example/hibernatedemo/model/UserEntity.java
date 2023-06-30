f1418285c86c3916078a944226c820c08d3f5399
TrungLe <trungle2001yf@gmail.com>
2023-06-30 17:22:49 +0700

    jbaonly

---
+++ src/main/java/com/example/hibernatedemo/model/UserEntity.java
@@ -0,0 +1,21 @@
+package com.example.hibernatedemo.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "user")
+@Data
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+    @Column(name = "age")
+    private int age;
+
+
+}
